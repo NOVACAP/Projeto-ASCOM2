a0da736cb6aeabb3bc24bfeb7fc6c13cd224f5c6
Gabriel Henrique Rabelo da Silva de Gusmão Lobo <gabriel.lobo@NOVACAP.SEDE>
2023-01-30 16:22:51 -0300

    ultima atualização antes da apresetação para o victor

--- funcYT.php
+++ funcYT.php
@@ -13,13 +13,13 @@ foreach($my_posts as $post) {
     $i++?>
 
     <?php $link = get_post_meta($post->ID, 'link',true);?>
-<div class="gallery">
-    <div class="gallery-container">
-        <iframe class="<?php echo "gallery-item-" .$i;?> <?php echo "gallery-item" ?>" src="<?php recebe($link);?>" data-index="<?php echo $i;?>"></iframe>
-        <h3 id="tvVidTitulo"><?php echo $post->post_title;?></h3>
+    <div class="gallery">
+        <div class="gallery-container">
+            <iframe class="<?php echo "gallery-item-" .$i;?> <?php echo "gallery-item" ?>" src="<?php recebe($link);?>" data-index="<?php echo $i;?>"></iframe>
+            <h3 id="tvVidTitulo"><?php echo $post->post_title;?></h3>
+        </div>
+        <?php } ?>
+        <div class="gallery-controls"></div>
     </div>
-    <?php } ?>
-    <div class="gallery-controls"></div>
-</div>
 <script src="<?php echo get_stylesheet_directory_uri(). '/testeSliderjs/script.js' ?>"></script>
 </section>
