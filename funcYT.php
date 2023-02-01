8accc04867dd237a308350c251acd0b1101ce276
Gabriel Henrique Rabelo da Silva de Gusmão Lobo <gabriel.lobo@NOVACAP.SEDE>
2023-02-01 14:02:03 -0300

    alterações do dia 01/02

--- funcYT.php
+++ funcYT.php
@@ -5,21 +5,26 @@
     'custom_fields' => 'link'
 ];
 
-$i = 0;?>
+$i = 0; ?>
 
 <?php
 $my_posts = get_posts($args);
-foreach($my_posts as $post) { 
-    $i++?>
-
-    <?php $link = get_post_meta($post->ID, 'link',true);?>
-    <div class="gallery">
-        <div class="gallery-container">
-            <iframe class="<?php echo "gallery-item-" .$i;?> <?php echo "gallery-item" ?>" src="<?php recebe($link);?>" data-index="<?php echo $i;?>"></iframe>
-            <h3 id="tvVidTitulo"><?php echo $post->post_title;?></h3>
+foreach ($my_posts as $post) {
+    $i++ ?>
+    <section>
+        <?php $link = get_post_meta($post->ID, 'link', true); ?>
+        <div class="gallery">
+            <div class="gallery-container">
+                <iframe
+                    class="<?php echo "gallery-item-" . $i; ?>                                         <?php echo "gallery-item" ?>"
+                    src="<?php recebe($link); ?>" data-index="<?php echo $i; ?>"></iframe>
+                <h3
+                    class="<?php echo "T-" . $i; ?>">
+                    <?php echo $post->post_title; ?>
+                </h3>
+            </div>
+            <div class="gallery-controls"></div>
         </div>
-        <?php } ?>
-        <div class="gallery-controls"></div>
-    </div>
-<script src="<?php echo get_stylesheet_directory_uri(). '/testeSliderjs/script.js' ?>"></script>
-</section>
+    </section>
+<?php } ?>
+<script src="<?php echo get_stylesheet_directory_uri() . '/testeSliderjs/script.js' ?>"></script>
