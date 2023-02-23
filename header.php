2f4d26f45c1e2c074c6e310bc5170cc483c60862
Gabriel Henrique Rabelo da Silva de Gusmão Lobo <gabriel.lobo@NOVACAP.SEDE>
2023-02-23 16:39:43 -0300

    searchbar inicio

--- header.php
+++ header.php
@@ -37,11 +37,9 @@
                         <use xlink:href="#bootstrap"></use>
                     </svg>
                 </a>
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                    <input type="search" class="form-control form-control-dark text-white" aria-label="Search"
-                        style="width: 197px !important;">
-                    <i class="align-items-center fa fa-search icon branco"></i>
-                </form>
+                <div>
+                    <?php get_search_form(); ?>
+                </div>
                 <div id="oi" class="p-2">
                     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                         <li>
